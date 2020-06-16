8a9ef5e9188f91891f7ac2594b9dc51750e2599b
Steven <mstevenwilliams@gmail.com>
2020-06-16 13:51:20 -0500

    Create shell for C client program.

---
+++ hashClient.c
@@ -0,0 +1,11 @@
+//Function declaration
+void sendFile(FILE *filePointer, int mySocket);
+
+int main(int argc , char *argv[]){
+
+	return 0;
+}
+
+void sendFile(FILE *filePointer, int mySocket){
+
+}
