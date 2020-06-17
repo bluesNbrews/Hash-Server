89886fcb43f3756b3aebf818bf64581b7103989d
Steven <mstevenwilliams@gmail.com>
2020-06-17 10:36:44 -0500

    Create shell for Python server program.

---
+++ hashServer.py
@@ -0,0 +1,30 @@
+def checkArgs(args=None):
+	pass
+
+def readFile(connection, fileSize, fileName):
+	pass
+
+def hashFile(fileName, hashAlgo):
+	pass
+
+#Hash the file (SHA1)
+def sha1(content):
+	pass
+
+#Hash the file (SHA256)
+def sha256(content):
+	pass
+
+#Hash the file (SHA512)
+def sha512(content):
+	pass
+
+#Hash the file (MD5)
+def md5(content):
+	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+    main()
