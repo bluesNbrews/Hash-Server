f03b1715c967f35067c29396229bd951b06bdbc5
Steven <mstevenwilliams@gmail.com>
2020-06-19 12:58:48 -0500

    Add source files, define preprocessor macros, and add command line input validation.

--- hashClient.c
+++ hashClient.c
@@ -1,7 +1,78 @@
+#include<arpa/inet.h>
+#include<ctype.h>
+#include<libgen.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>	
+#include<sys/socket.h>
+#include<unistd.h>
+
+#define BUFFER_SIZE 4096
+#define LINE_SIZE 4096
+
+//Keep track of the number of bytes sent
+ssize_t total = 0;
+
 //Function declaration
 void sendFile(FILE *filePointer, int mySocket);
 
 int main(int argc , char *argv[]){
+
+	char* fileName;
+    char* filePath;
+    FILE *filePointer;
+    char* hashingAlgorithm;
+    int index;
+    char* ipAddress;
+    int mySocket;
+    ssize_t numBytesRecv;
+    char sendBuffer[BUFFER_SIZE] = {0};
+    char recvBuffer[BUFFER_SIZE] = {0};
+    int optionChar; 
+    unsigned long portNum = 2345;
+    struct sockaddr_in server;
+    opterr = 0;
+
+	//Input validation for the number of arguments
+	if (argc < 4){
+        puts("Usage: ./hashClient [-p port] <IP Address> <Hashing Algorithm> <File Path>\n");
+        puts("Example: ./hashClient [-p 2345] 127.0.0.1 sha1 /Desktop/file.txt\n");
+        return 1;
+    }
+
+    //Input validation for optional port number and additional arguments
+    while ((optionChar = getopt (argc, argv, "p:")) != -1)
+      switch (optionChar){
+        case 'p':
+            portNum = strtoul(optarg, NULL, 10);
+            if (portNum <= 0 || portNum >= 65536){
+                puts("Please enter a valid port number (0-65535).");
+                return 1;
+            }
+          break;
+        case '?':
+          if (optopt == 'p')
+            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+          else if (isprint (optopt))
+            fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+          else
+            fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+          return 1;
+        default:
+          abort ();
+    }
+    ipAddress = argv[optind];
+    hashingAlgorithm = argv[optind + 1];
+
+    //Make sure that the IP address and hashing algorithm variables aren't null
+    if (ipAddress == NULL){
+        puts("Something is wrong with the IP Address.\n");
+        return 1;
+    }
+    if (hashingAlgorithm == NULL){
+        puts("Something is wrong with the Hashing Algorithm.\n");
+        return 1;
+    }
 
 	return 0;
 }
